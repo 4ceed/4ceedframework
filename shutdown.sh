2f18b52e2e8c408b6090dda68f092f4cdb64aedd
Phuong Nguyen <pvnguye2@illinois.edu>
2016-10-04 00:25:36 -0500

    Unify startup & shutdown processes

--- shutdown.sh
+++ shutdown.sh
@@ -1,33 +1,55 @@
 #!/bin/bash
 
-source custom.conf
-
 # Setup environment variables
+source custom.conf
 export KUBECTL
-$KUBECTL delete namespace 4ceed
+
+# Get shutdown target
+if [ "$1" != "" ]; then
+	if [ "$1" != "all" ] && [ "$1" != "tools" ] && [ "$1" != "extractors" ] && \
+	   [ "$1" != "curator" ] && [ "$1" != "uploader" ]; then
+		echo 'Invalid shutdown target (all|tools|extractors|curator|uploader).'
+		exit 1
+	else
+		TARGET="$1"
+	fi
+else
+    TARGET="all"
+fi
 
 # Shutdown 4CeeD's uploader 
-echo "Shutting down 4CeeD's uploader..."
-$KUBECTL delete svc t2c2uploadersvc
-$KUBECTL delete rc t2c2uploader
+if [ "$TARGET" == "uploader" ] || [ "$TARGET" == "all" ]; then
+	echo "Shutting down 4CeeD's uploader..."
+	$KUBECTL delete svc t2c2uploadersvc --namespace=4ceed
+	$KUBECTL delete rc t2c2uploader --namespace=4ceed
+fi
 
 # Shutdown 4CeeD's curator 
-echo "Shutting down 4CeeD's curator..."
-$KUBECTL delete svc t2c2curatorsvc
-$KUBECTL delete rc t2c2curator
+if [ "$TARGET" == "curator" ] || [ "$TARGET" == "all" ]; then
+	echo "Shutting down 4CeeD's curator..."
+	$KUBECTL delete svc t2c2curatorsvc --namespace=4ceed
+	$KUBECTL delete rc t2c2curator --namespace=4ceed
+fi
 
 # Extractors
-echo "Shutting down 4CeeD's extractors..."
-$KUBECTL delete rc dm3-extractor
-$KUBECTL delete rc image-preview-extractor
-$KUBECTL delete rc sem-extractor
-$KUBECTL delete rc xray-extractor
+if [ "$TARGET" == "extractors" ] || [ "$TARGET" == "all" ]; then
+	echo "Shutting down 4CeeD's extractors..."
+	$KUBECTL delete rc dm3-extractor --namespace=4ceed
+	$KUBECTL delete rc image-preview-extractor --namespace=4ceed
+	$KUBECTL delete rc sem-extractor --namespace=4ceed
+	$KUBECTL delete rc xray-extractor --namespace=4ceed
+fi
 
 # Setup tools
-echo "Shutting down 4CeeD's dependent tools..."
-$KUBECTL delete svc elasticsearch 
-$KUBECTL delete rc es 
-$KUBECTL delete svc mongo 
-$KUBECTL delete rc mongo-controller 
-$KUBECTL delete svc rabbitmq-service 
-$KUBECTL delete rc rabbitmq-controller 
+if [ "$TARGET" == "tools" ] || [ "$TARGET" == "all" ]; then
+	echo "Shutting down 4CeeD's dependent tools..."
+	$KUBECTL delete svc elasticsearch --namespace=4ceed
+	$KUBECTL delete rc es --namespace=4ceed
+	$KUBECTL delete svc mongo --namespace=4ceed
+	$KUBECTL delete rc mongo-controller --namespace=4ceed
+	$KUBECTL delete svc rabbitmq-service --namespace=4ceed
+	$KUBECTL delete rc rabbitmq-controller --namespace=4ceed
+fi
+
+# Delete namespace
+$KUBECTL delete namespace 4ceed
--- startup.sh
+++ startup.sh
@@ -1,27 +1,95 @@
 #!/bin/bash
 
-source custom.conf
-
 # Setup environment variables
+source custom.conf
 export KUBECTL
+
+# Create 4ceed namespace
 $KUBECTL create namespace 4ceed
 
+# Get startup target
+if [ "$1" != "" ]; then
+	if [ "$1" != "all" ] && [ "$1" != "tools" ] && [ "$1" != "extractors" ] && \
+	   [ "$1" != "curator" ] && [ "$1" != "uploader" ]; then
+		echo 'Invalid startup target (all|tools|extractors|curator|uploader).'
+		exit 1
+	else
+		TARGET="$1"
+	fi
+else
+    TARGET="all"
+fi
+
 # Setup tools
-echo "Deploying 4CeeD's dependent tools..."
-cd tools
-./tools-deploy.sh
+if [ "$TARGET" == "tools" ] || [ "$TARGET" == "all" ]; then
+	echo "Deploying 4CeeD's dependent tools..."
+	# Deploy RabbitMQ
+	$KUBECTL create -f tools/rabbitmq/rabbitmq-controller.yaml
+	$KUBECTL create -f tools/rabbitmq/rabbitmq-service.yaml
+
+	# Deploy MongoDB 
+	$KUBECTL create -f tools/mongodb/mongo-controller.yaml
+	$KUBECTL create -f tools/mongodb/mongo-service.yaml
+
+	# Deploy Elasticsearch 
+	$KUBECTL create -f tools/elasticsearch/service-account.yaml
+	$KUBECTL create -f tools/elasticsearch/es-rc.yaml
+	$KUBECTL create -f tools/elasticsearch/es-svc.yaml 
+fi
 
 # Setup 4CeeD curator 
-echo "Deploying 4CeeD's curator..."
-cd ../curator 
-./curator-deploy.sh $REBUILD_IMAGES
+if [ "$TARGET" == "curator" ] || [ "$TARGET" == "all" ]; then
+	echo "Deploying 4CeeD's curator..."
+
+	if [ "$BUILD_DOCKER_IMG" = "true" ]; then
+		cd curator
+		echo 'Building Curator Docker image from source...'
+		# Clone the latest version of 4CeeD curator:
+		git clone https://bitbucket.org/todd_n/4ceedcurator.git
+
+		# Copy necessary files to 4ceedcurator folder:
+		cp Dockerfile curator-start.sh libjnotify64.so 4ceedcurator/
+		mv 4ceedcurator/libjnotify64.so 4ceedcurator/libjnotify.so
+
+		# Build 4CeeD curator Docker image:
+		docker build -t t2c2/4ceedcurator 4ceedcurator/
+		cd ..
+	else
+		echo 'Using prebuilt Curator Docker image...'
+	fi
+
+	# Deploy curator to Kubernetes:
+	$KUBECTL create -f curator/curator-rc.yaml
+	$KUBECTL create -f curator/curator-svc.yaml
+fi
+
 
 # Setup extractors
-echo "Deploying 4CeeD's extractors..."
-cd ../extractors
-$KUBECTL create -f .
+if [ "$TARGET" == "extractors" ] || [ "$TARGET" == "all" ]; then
+	echo "Deploying 4CeeD's extractors..."
+	$KUBECTL create -f extractors/
+fi
 
 # Setup 4CeeD's uploader 
-echo "Deploying 4CeeD's uploader..."
-cd ../uploader 
-./uploader-deploy.sh $REBUILD_IMAGES
+if [ "$TARGET" == "uploader" ] || [ "$TARGET" == "all" ]; then
+	echo "Deploying 4CeeD's uploader..."
+	if [ "$BUILD_DOCKER_IMG" = "true" ]; then
+		cd uploader
+		echo 'Building Uploader Docker image from source...'
+		# Clone the latest version of 4CeeD curator:
+		git clone git@bitbucket.org:smkctk/4ceeduploader.git
+
+		# Copy necessary files to 4ceedcurator folder:
+		cp Dockerfile 4ceeduploader/
+
+		# Build 4CeeD curator Docker image:
+		docker build -t t2c2/4ceeduploader 4ceeduploader/
+		cd ..
+	else
+		echo 'Using prebuilt Uploader Docker image...'
+	fi
+
+	# Deploy curator to Kubernetes:
+	$KUBECTL create -f uploader/uploader-rc.yaml
+	$KUBECTL create -f uploader/uploader-svc.yaml
+fi
