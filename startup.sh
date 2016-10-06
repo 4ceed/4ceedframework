117b2eaaaf3ed176b0029efef85eb28954fd631f
Phuong Nguyen <phuongnv87@gmail.com>
2016-10-06 14:47:20 -0500

    Add AFM extractor, extractor registration script

--- startup.sh
+++ startup.sh
@@ -22,24 +22,31 @@ fi
 
 # Setup tools
 if [ "$TARGET" == "tools" ] || [ "$TARGET" == "all" ]; then
-	echo "Deploying 4CeeD's dependent tools..."
+    echo
+	echo "========Deploying 4CeeD's dependent tools...========"
+	echo
 	# Deploy RabbitMQ
 	$KUBECTL create -f tools/rabbitmq/rabbitmq-controller.yaml
 	$KUBECTL create -f tools/rabbitmq/rabbitmq-service.yaml
+	sleep 5
 
 	# Deploy MongoDB 
 	$KUBECTL create -f tools/mongodb/mongo-controller.yaml
 	$KUBECTL create -f tools/mongodb/mongo-service.yaml
+	sleep 5
 
 	# Deploy Elasticsearch 
 	$KUBECTL create -f tools/elasticsearch/service-account.yaml
 	$KUBECTL create -f tools/elasticsearch/es-rc.yaml
 	$KUBECTL create -f tools/elasticsearch/es-svc.yaml 
+	sleep 5
 fi
 
 # Setup 4CeeD curator 
 if [ "$TARGET" == "curator" ] || [ "$TARGET" == "all" ]; then
-	echo "Deploying 4CeeD's curator..."
+    echo
+	echo "========Deploying 4CeeD's curator...========"
+	echo
 
 	if [ "$BUILD_DOCKER_IMG" = "true" ]; then
 		cd curator
@@ -61,18 +68,30 @@ if [ "$TARGET" == "curator" ] || [ "$TARGET" == "all" ]; then
 	# Deploy curator to Kubernetes:
 	$KUBECTL create -f curator/curator-rc.yaml
 	$KUBECTL create -f curator/curator-svc.yaml
+	sleep 5
 fi
 
 
 # Setup extractors
 if [ "$TARGET" == "extractors" ] || [ "$TARGET" == "all" ]; then
-	echo "Deploying 4CeeD's extractors..."
-	$KUBECTL create -f extractors/
+    echo
+	echo "========Deploying 4CeeD's extractors...========"
+	echo
+
+	$KUBECTL create -f extractors/dm3-extractor.yaml
+	$KUBECTL create -f extractors/image-preview-extractor.yaml
+	$KUBECTL create -f extractors/sem-extractor.yaml
+	$KUBECTL create -f extractors/xray-extractor.yaml
+	$KUBECTL create -f extractors/afm-extractor.yaml
+	sleep 5
 fi
 
 # Setup 4CeeD's uploader 
 if [ "$TARGET" == "uploader" ] || [ "$TARGET" == "all" ]; then
-	echo "Deploying 4CeeD's uploader..."
+    echo
+	echo "========Deploying 4CeeD's uploader...========"
+	echo
+
 	if [ "$BUILD_DOCKER_IMG" = "true" ]; then
 		cd uploader
 		echo 'Building Uploader Docker image from source...'
