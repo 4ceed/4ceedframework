5bb484d4b0d1efabd964296d8e63475cae2bd12a
Phuong Nguyen <phuongnv87@gmail.com>
2017-11-02 06:06:54 +0000

    Allow to register individual extractor

--- register_extractors.sh
+++ register_extractors.sh
@@ -3,13 +3,23 @@
 # Setup environment variables
 source custom.conf
 
-# Register extractors in 'extractors/' dir
-for extractor_file in $(find extractors/ -name '*.json'); do
-    echo "Register extractor ${extractor_file}..."
-    extractor_info=$(cat $extractor_file)
-    curl -X POST \
-        -H "Content-Type: application/json" \
-        -d "${extractor_info}" \
-        -u $ADMIN_EMAIL \
-        "${CURATOR_ADDR}/api/extractors"
-done
+if [ "$1" != "" ] ; then
+	extractor_info=$(cat $1)
+	curl -k -X POST \
+		-H "Content-Type: application/json" \
+		-d "${extractor_info}" \
+		-u $ADMIN_EMAIL \
+		"${CURATOR_ADDR}/api/extractors"
+else
+	# Register extractors in 'extractors/' dir
+	for extractor_file in $(find extractors/ -name '*.json'); do
+	    echo "Register extractor ${extractor_file}..."
+	    extractor_info=$(cat $extractor_file)
+	    curl -k -X POST \
+		-H "Content-Type: application/json" \
+		-d "${extractor_info}" \
+		-u $ADMIN_EMAIL \
+		"${CURATOR_ADDR}/api/extractors"
+	done
+fi
+
